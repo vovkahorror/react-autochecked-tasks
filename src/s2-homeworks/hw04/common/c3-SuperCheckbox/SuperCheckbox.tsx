e299a08c231300b2d7ce08e18309b1671e125190
Volodymyr Yaremchak <vovkahorror@gmail.com>
2022-10-05 20:46:30 +0300

    fixed bug with checkbox

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -28,6 +28,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
+        onChange && onChange(e);
         onChangeChecked && onChangeChecked(e.currentTarget.checked);
     };
 
